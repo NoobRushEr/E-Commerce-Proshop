84924402b08ceb10ea9463cecb47ddeddde39bfb
Dipesh Ghag <dipeshghag1234@gmail.com>
2024-03-13 18:46:09 +0530

    Added Frontend Homescreen and productscreen

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <Navbar bg='dark' variant='dark' expand="lg" collapseOnSelect>
+                    <Container fluid>
+                        <LinkContainer to='/'>
+                            <Navbar.Brand>ProShop</Navbar.Brand>
+                        </LinkContainer>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="mr-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                            >
+                                <LinkContainer to="/cart">
+                                    <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to="/login">
+                                    <Nav.Link><i className='fas fa-user'></i>Login</Nav.Link>
+                                </LinkContainer>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+        </div>
+    )
+}
+
+export default Header
