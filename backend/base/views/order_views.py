6b4a8acb515d777481b28d6f27061c010b29c51d
NoobRushEr <dipeshghag1234@gmail.com>
2024-03-19 08:16:46 +0530

    Added Get order by id

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -53,3 +53,20 @@ def addOrderItems(request):
         product.save()
         serializer = OrderSerializer(order, many=False)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+    user = request.user
+
+    order = Order.objects.get(_id=pk)
+
+    try:
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            return Response({'detail': 'Not Authorized to view this order'}, status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail': 'Order Does not exist'}, status=status.HTTP_400_BAD_REQUEST)
